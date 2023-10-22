5c786360178946f79caf97f1b6e875aa993df1b4
k80 <katiegambrose@gmail.com>
2023-10-21 19:18:21 -0500

    fixed so that ids are now classes in about section

--- clickabout.js
+++ clickabout.js
@@ -1,9 +1,11 @@
-document.getElementById('about-heading').addEventListener('click', function() {
-    const content = document.getElementById('about-content');
-    
-    if (content.style.maxHeight === '0px' || content.style.maxHeight === '') {
-        content.style.maxHeight = content.scrollHeight + 'px';
-    } else {
-        content.style.maxHeight = '0px';
-    }
+document.querySelectorAll('.about-heading').forEach(heading => {
+    heading.addEventListener('click', function() {
+        const content = this.nextElementSibling;
+
+        if (content.style.maxHeight === '0px' || content.style.maxHeight === '') {
+            content.style.maxHeight = content.scrollHeight + 'px';
+        } else {
+            content.style.maxHeight = '0px';
+        }
+    });
 });
